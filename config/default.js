2d81643f60ec3cd72bbe7967a4a5105cda83588a
guitar19823 <gandon111@gmail.com>
2019-02-21 18:32:21 +0300

    version 1

--- config/default.js
+++ config/default.js
@@ -3,17 +3,8 @@ const path = require('path');
 module.exports = {
   // secret data can be moved to env variables
   // or a separate config
-  secret: process.env.SECRET || 'mysecret',
+  port: process.env.PORT || 3000,
+  secret: 'mysecret',
   root: process.cwd(),
-  templatesRoot: path.join(process.cwd(), 'templates'),
-  crypto: {
-    hash: {
-      length: 128,
-      iterations: 10
-    }
-  },
-  server: {
-    host: 'http://localhost',
-    port: 3000,
-  }
+  templatesRoot: path.join(process.cwd(), 'templates')
 };
